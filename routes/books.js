33077607d6af757087c5e75db72d8a8942995c9a
hafidzniioman <hafidzniioman@gmail.com>
2020-07-26 23:35:31 +0700

    File Upload Database but filepond not working and not push to heroku

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
 const Books = require("../models/book");
 const Inventory = require("../models/inventory");
-const uploadPath = path.join('public', Books.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) =>{
-        callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-})
-
+/
 // All Books Route
 router.get("/", async (req, res) => {
     let query = Books.find()
@@ -38,37 +28,27 @@ router.get("/", async (req, res) => {
 });
 
 // New Book route
-router.get("/new",  async (req, res) => {
+router.get("/new", async (req, res) => {
     renderNewPage(res, new Books())
 });
 
 // Create Book Route
-router.post("/", upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+router.post("/", async (req, res) => {
     const book = new Books({
         title: req.body.title,
         inventory: req.body.inventory,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     })
+    saveCover(book, req.body.cover)
     try{
         const newBook = await book.save()
         res.redirect('books')
     } catch {
-        if(book.coverImageName != null){
-            removeBookCover(book.coverImageName)
-        } 
         renderNewPage(res, book, true)
     }
 });
-
-function removeBookCover(fileName){
-    fs.unlink(path.join(uploadPath, fileName), err => {
-        if(err) console.error(err)
-    })
-}
 
 async function renderNewPage(res, book, hasError = false){
     try {
@@ -82,6 +62,15 @@ async function renderNewPage(res, book, hasError = false){
       } catch {
         res.redirect("/books");
       }
+}
+
+function saveCover(book, coverEncoded){
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
 }
 
 module.exports = router;
